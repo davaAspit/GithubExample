cb95a79ba9cde3c3bcd6b1eb02aaf7d72bff3344
Daniel Valsby-Koch <dava@aspit.dk>
2019-06-06 13:01:06 +0200

    Added RazorPage example

---
+++ LearningRazorPages.Web/Pages/Errors/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LearningRazorPages.Web.Pages.Errors
+{
+    public class IndexModel : PageModel
+    {
+        public string ErrorMessage { get; set; }
+
+        public void OnGet(int id)
+        {
+            switch (id)
+            {
+                case 404:
+                    ErrorMessage = "Siden blev ikke fundet...";
+                    break;
+                default:
+                    ErrorMessage = "Der opstod en ukendt fejl...";
+                    break;
+            }
+        }
+    }
+}
